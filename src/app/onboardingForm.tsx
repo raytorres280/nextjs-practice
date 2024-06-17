ea61b2c6a88cc26a9d79407b2d2e9afad6ac4a4d
ray torres <raytorres280@gmail.com>
2024-06-16 23:47:36 -0400

    remove some boilerplate

--- src/app/onboardingForm.tsx
+++ src/app/onboardingForm.tsx
@@ -3,13 +3,13 @@ export default function OnboardingForm() {
         <div className="w-full max-w-xs">
             <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
                         Username
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
                 </div>
                 <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
                         Password
                     </label>
                     <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
